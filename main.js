8420df0a8a2064ea83267cf2803d35e0e3cbaa0a
cheyan <375363996@qq.com>
2021-10-28 22:59:35 +0800

    数据类型-整数

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 document.write('外部文件的写入\n')
-console.log("外部文件的",document)
+console.log("外部文件的\n", document)
+
+//标识符,变量支持，数字Number(包括整数和浮点数)、字符串String、Bboolean、Null、Undefined、Object(引用数据类型)
+//数字中：Infinity表示无穷大，超过最大阿表示范围
+//浮点数的运算会失去精确性，因为二进制
+var a;
+console.log('a的类型是：', typeof a);
+console.log(Number.MIN_VALUE)
